f26d5a6d9b87793b92eb0fd0f0ba59ac0e76bb40
Ugur <ugurarslanpeace@gmail.com>
2024-04-08 19:49:29 +0300

    initial commit

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,31 @@
+import { useRef } from "react";
+
+import { FaBars, FaTimes } from "react-icons/fa";
+
+function NavBar() {
+  const navRef = useRef();
+
+  const showNavBar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
+
+  return (
+    <header>
+      <h3>Logo</h3>
+      <nav ref={navRef}>
+        <a href="/#">Home</a>
+        <a href="/#">My work</a>
+        <a href="/#">Blog</a>
+        <a href="/#">About me</a>
+        <button className="nav-btn nav-close-btn" onClick={showNavBar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <button className="nav-btn" onClick={showNavBar}>
+        <FaBars />
+      </button>
+    </header>
+  );
+}
+
+export default NavBar;
